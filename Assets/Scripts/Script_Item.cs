cfd27834cdb209bda7bed2e94a0bf39321d46638
DepressedCookie <vipc.1224@live.ca>
2020-10-25 03:01:57 -0400

    Added Items

---
+++ Assets/Scripts/Script_Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Item : MonoBehaviour
+{
+    public bool Coin;
+    public bool Heart;
+
+    private void Start()
+    {
+        transform.position = new Vector2(Random.Range(-2.0f, 2.0f), transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DoStuff(collision);
+        }
+    }
+
+    public void DoStuff(Collider2D col)
+    {
+        if (Coin)
+        {
+            GameObject.FindGameObjectWithTag("GameMana").GetComponent<Script_GameMana>().AddScore(10);
+            col.GetComponent<AudioSource>().PlayOneShot(col.GetComponent<AudioSource>().clip);
+        }
+        if (Heart)
+        {
+            GameObject.FindGameObjectWithTag("GameMana").GetComponent<Script_GameMana>().AddLife();
+        }
+        gameObject.SetActive(false);
+    }
+}
