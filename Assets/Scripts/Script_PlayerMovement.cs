525364a933a0e3a9da66f5bd7b6020d5443e726b
DepressedCookie <vipc.1224@live.ca>
2020-10-25 03:03:35 -0400

    Added player movement

---
+++ Assets/Scripts/Script_PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+
+public class Script_PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float MoveSpeed = 3.0f;
+
+    Animator anim;
+    public GameObject Tongue;
+
+    bool CanFire = true;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAndAttack();
+    }
+
+    void MoveAndAttack()
+    {
+        foreach (var touch in Input.touches)
+        {
+            var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            bool right = worldTouch.x > 0.0f && transform.position.x < 2.5f;
+            bool left = worldTouch.x < -0.0f && transform.position.x > -2.5f;
+            bool fire = worldTouch.y > -3.0f;
+
+
+            // Move right
+            if (right && !fire)
+            {
+                transform.Translate(MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
+            }
+
+            // Move left
+            if(left && !fire)
+            {
+                transform.Translate(-MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
+            }
+
+            if(fire && CanFire)
+            {
+                CanFire = false;
+                anim.SetTrigger("Fire");
+                Tongue.SetActive(true);
+                Invoke("DelayStopFire", 0.5f);
+                Invoke("DelayCanFire", 1.0f);
+            }
+        }
+    }
+
+    void DelayStopFire()
+    {
+        Tongue.SetActive(false);
+    }
+
+    void DelayCanFire()
+    {
+        CanFire = true;
+    }
+}
