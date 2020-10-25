1d71705a85c7cfd766185968aef798e2937c8ef9
DepressedCookie <vipc.1224@live.ca>
2020-10-25 03:02:58 -0400

    Added Game manager and scene manager

---
+++ Assets/Scripts/Script_GameMana.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_GameMana : MonoBehaviour
+{
+    public GameObject LivesObj;
+    public GameObject ScoreObj;
+    public int Lives = 3;
+    public int Score = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddLife()
+    {
+        if(Lives < 3)
+        {
+            Lives++;
+            UpdateUI();
+        }
+    }
+    public void RemoveLife()
+    {
+        if(Lives > 0)
+        {
+            Lives--;
+            UpdateUI();
+        }
+        if(Lives == 0)
+        {
+            SceneManager.LoadScene("Lvl_Game02");
+        }
+    }
+    public void AddScore(int score)
+    {
+        Score += score;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        LivesObj.GetComponent<TextMeshProUGUI>().SetText("Lives: {0}", Lives);
+        ScoreObj.GetComponent<TextMeshProUGUI>().SetText("Score: {0}", Score);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("TEST");
+        if(scene.name == "Lvl_Game02")
+        {
+            GameObject.FindGameObjectWithTag("EndScore").GetComponent<TextMeshProUGUI>().SetText("Score: {0}", Score);
+        }
+        if(scene.name == "Lvl_Start")
+        {
+            Lives = 3;
+            Score = 0;
+        }
+    }
+
+    public void MakeItHarder()
+    {
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Map"))
+        {
+            g.GetComponent<Script_MapScroll>().verticalSpeed *= 1.5f;
+        }
+    }
+}
---
+++ Assets/Scripts/Script_SceneMana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_SceneMana : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().name == "Lvl_Game02")
+        {
+            GameObject.FindGameObjectWithTag("EndScore").GetComponent<TextMeshProUGUI>().SetText("Score: {0}", GameObject.FindGameObjectWithTag("GameMana").GetComponent<Script_GameMana>().Score);
+            Destroy(GameObject.FindGameObjectWithTag("GameMana"));
+        }
+    }
+}
