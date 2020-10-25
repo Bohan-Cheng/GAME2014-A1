2a664421ddcd926346d914faf32a0c6e9901ccf7
DepressedCookie <vipc.1224@live.ca>
2020-10-25 03:03:48 -0400

    Added Player attack

---
+++ Assets/Scripts/Script_Tongue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Tongue : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            collision.gameObject.SetActive(false);
+            GameObject.FindGameObjectWithTag("GameMana").GetComponent<Script_GameMana>().AddScore(15);
+        }
+        
+        if(collision.tag == "Item")
+        {
+            collision.GetComponent<Script_Item>().DoStuff(GetComponent<BoxCollider2D>());
+        }
+    }
+}
